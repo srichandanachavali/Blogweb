89775480ff58fe862b3f021d40c800eeab007610
srichandanachavali <srichandanachavali@gmail.com>
2025-11-03 19:46:51 +0530

    Configure project for production deployment

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -1,19 +1,44 @@
 from pathlib import Path
+import os  # <-- ADD THIS
+import dj_database_url  # <-- ADD THIS
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-change-me'
 
-DEBUG = True
+# --- 1. SECRET_KEY IS NOW HIDDEN ---
+# It will get the key from an environment variable.
+# If it can't find it (like on your laptop), it uses a *different*, unsafe
+# key just for local testing.
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 
+    'django-insecure-this-is-a-backup-key-for-local-dev-only'
+)
 
-ALLOWED_HOSTS: list[str] = []
+# --- 2. DEBUG IS SET SMARTLY ---
+# It's 'False' unless an environment variable 'DEBUG' is set to 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
+
+# --- 3. ALLOWED_HOSTS IS READY ---
+# Render will give you a URL like 'my-site.onrender.com'.
+# You MUST add it to this list.
+ALLOWED_HOSTS = ['127.0.0.1']
+
+# This will be your live site's URL
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
+# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',  # <-- ADD THIS
     'django.contrib.staticfiles',
     'taggit',
     'blog',
@@ -22,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- ADD THIS
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -50,46 +76,42 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blog_project.wsgi.application'
 
+
+# --- 4. DATABASE IS NOW PRODUCTION-READY ---
+# This will use your local db.sqlite3 file by default.
+# BUT, if it finds a 'DATABASE_URL' variable (which Render will provide),
+# it will automatically connect to your new PostgreSQL database.
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
+
+# Password validation
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # ... (your validators are fine) ...
 ]
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
+# --- 5. STATIC & MEDIA FILES ARE READY ---
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# --- THIS IS THE NEW LINE YOU WERE MISSING ---
-# This tells Django where to find your *source* static files
 STATICFILES_DIRS = [BASE_DIR / 'static']
+# This tells Whitenoise to be smart about caching files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+# WARNING: Media files will NOT work on Render yet.
+# That's a separate fix using a service like Cloudinary or S3.
 
+# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'post_list'
 LOGOUT_REDIRECT_URL = 'post_list'
