8f3d757b839bd40f4c0da882447741d33497f0f2
srichandanachavali <srichandanachavali@gmail.com>
2025-11-05 12:42:38 +0530

    Fixed Profile model to use CloudinaryField

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-# Make sure User is imported, or use settings.AUTH_USER_MODEL
-from django.contrib.auth.models import User 
+from cloudinary.models import CloudinaryField  # <-- 1. IMPORT THIS
+
+# Using settings.AUTH_USER_MODEL is good practice
+User = settings.AUTH_USER_MODEL 
 
 class Profile(models.Model):
     user = models.OneToOneField(
@@ -11,16 +13,21 @@ class Profile(models.Model):
         on_delete=models.CASCADE,
         related_name='profile',
     )
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     
-    # --- THIS IS THE NEW FIELD ---
-    # This will store all the User accounts that are "following" this profile.
+    # --- THIS IS THE FIXED FIELD ---
+    image = CloudinaryField(
+        'image',
+        folder='profile_pics',   # <-- 3. SET THE FOLDER
+        default='v1730825368/profile_pics/default_k3mjna.jpg'  # <-- 4. USE A CLOUDINARY DEFAULT
+    )
+    # --- END FIXED FIELD ---
+    
+    # This field is correct
     followers = models.ManyToManyField(
         settings.AUTH_USER_MODEL, 
         related_name='following', 
         blank=True
     )
-    # ---------------------------
 
     def __str__(self) -> str:
         return f'{self.user.username} Profile'
@@ -29,3 +36,4 @@ class Profile(models.Model):
 def ensure_user_profile(sender, instance, created, **kwargs):
     """Create a profile for every user and backfill missing ones."""
     Profile.objects.get_or_create(user=instance)
+    
